da811bba81083e82b0138b33a068ba38357df2a0
Jarl Holmberg Lindquist <jarllindquist@gmail.com>
2016-04-02 09:52:39 +0200

    alter server.js , created so it is possible to list all fields in test db:n

--- server.js
+++ server.js
@@ -6,8 +6,48 @@ bodyParser = require('body-parser'),
 mongodb = require('mongodb'),
 mongoose = require('mongoose');
 
+app.use(bodyParser.urlencoded({
+	extended: true
+}));
 
+// Connection URl. This is where your mongodb is running.
+var url = 'mongodb://localhost:27017/test';
 
+//
+var MongoClient = mongodb.MongoClient;
+
+ // use connection method to connect to the server
+MongoClient.connect(url, function(err, db){
+	if(err){
+		console.log("Cant connect to mongodb because:", err);
+	}
+	else{
+		console.log("Connection established to " , url );
+
+		// collection is an database ? 
+		var collection = db.collection('test');
+		// var data = collection.insert({name: "albert svanberg", ocuppation: "speaker"});
+		// collection.find();
+		db.close();
+	}
+});
+MongoClient.connect(url, function(err, db) {
+  finddata(db, function() {
+      db.close();
+  });
+});
+
+var finddata = function(db, callback) {
+   var cursor =db.collection('test').find( );
+   cursor.each(function(err, data) {
+      
+      if (data != null) {
+         console.log(data);
+      } else {
+         callback();
+      }
+   });
+};
 
 app.use(express.static('public'));
 
